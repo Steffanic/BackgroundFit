a56ca03cb4df7643a0eb89903106b87ee3eb7ade
Steffanic <collidingpaticles@gmail.com>
2021-06-23 13:14:02 -0400

    Adding new model from big ugly equation paper

---
+++ PatModel.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def background_level( delta_phi:float
+                    , params:dict
+                    , trigger_yield:int
+                    , associated_yield:int
+                    , reaction_plane_order:int
+                    , azimuth_region_width:float
+                    , azimuth_region_center:float):
+                    r''' Calculates background level \tilde{B} and effective \tilde{v}^t_n, \tilde{w}^t_n; returns B(\Delta \phi)
+
+                        From the paper: https://arxiv.org/pdf/1802.01668.pdf. This function computes the value of equation A.30.
+
+                        Keyword Arguments:
+                        delta_phi -- phi_t - phi_a
+                        params -- Dictionary containing values for {"v_1t":, "v_2t":, "v_3t":, "v_4t":, "v_6t", "v_1a":, "v_2a":, "v_3a":, "v_4a":, "v_6a"}
+                        trigger_yield -- number of triggers in this azimuthal region
+                        associated_yield -- number of associated particles in this azimuthal region
+                        
+                    '''
